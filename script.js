2c0c84ca9cf5d7d49b1a6386e104ede7777bda5a
renatasousa3 <renatacsousa45@gmail.com>
2023-12-07 09:39:52 -0300

    upload
    upload de arquivos

---
+++ script.js
@@ -0,0 +1,14 @@
+AOS.init();
+
+/* barra de rolagem */
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
