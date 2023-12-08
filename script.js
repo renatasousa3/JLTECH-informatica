adbcb4617373a3e15f090c6afd72f229e4458d80
renatasousa3 <renatacsousa45@gmail.com>
2023-12-07 22:51:48 -0300

    Update
    update

--- script.js
+++ script.js
@@ -16,21 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// Função para rolar de volta para o topo da página
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-}
 
-// Função para mostrar/ocultar o botão "Retornar ao Menu" conforme a rolagem
-window.onscroll = function () {
-    const button = document.getElementById("back-to-top");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        button.style.display = "block";
-    } else {
-        button.style.display = "none";
-    }
-}
+
+
 
